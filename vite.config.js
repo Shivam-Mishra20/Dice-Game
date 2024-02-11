c3a180a45af9600f0a3cea1e26eb779076fb225f
Shivam-Mishra20 <shivammishra912998@gmail.com>
2024-02-11 20:43:32 +0530

    changes in mainjsx

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Shivam-Mishra20.github.io/Dice-Game',
+  base: '/Dice-Game/',
   plugins: [react()],
 })
